f0776387f37743ca3fe65aaa07d0e7b6b04013e2
Liam <lr98524@email.vccs.edu>
2025-02-18 16:15:36 -0500

    Finally made Vite.config.js work

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-export default defineConfig((
+export default defineConfig({
     build: { 
         outDir: "docs",
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "index.html"),
-                about: resolve{__dirname, "about/index.html"},
+                about: resolve(__dirname, "about/index.html"),
             },
         },
     },
-));
+});
